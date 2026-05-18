aabaed7983738c3566f83a1e458fe6e1351775b4
imebd <imanelbridi5@email.com>
2026-05-18 22:24:24 +0200

    fonctionnement du api produits

--- backend/server.js
+++ backend/server.js
@@ -11,15 +11,35 @@ app.get('/', (req, res) => {
 });
 
 const PORT = 5000;
+app.get('/api/products', async (req, res) => {
+    try {
+        const [rows] = await db.query('SELECT * FROM products');
+        res.json(rows); 
+    } catch (err) {
+        res.status(500).json({ error: "Erreur lors de la récupération des produits" });
+    }
+});
+
+app.post('/api/register', async (req, res) => {
+    const { username, email, password } = req.body;
+    try {
+        const query = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+        await db.query(query, [username, email, password]);
+        res.status(201).json({ message: "Utilisateur créé avec succès !" });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Erreur lors de l'inscription" });
+    }
+});
 app.listen(PORT, () => {
     console.log(`Serveur demarre sur le port ${PORT}`);
 });
 // Test visuel de la connexion
 db.query("SELECT 1")
     .then(() => {
-        console.log("✅ BRAVO : La connexion à MySQL (XAMPP) est établie !");
+        console.log(" BRAVO : La connexion à MySQL (XAMPP) est établie !");
     })
     .catch((err) => {
-        console.log("❌ ERREUR : Connexion impossible. Vérifie XAMPP ou ton fichier .env");
+        console.log(" ERREUR : Connexion impossible. Vérifie XAMPP ou ton fichier .env");
         console.error(err);
     });
